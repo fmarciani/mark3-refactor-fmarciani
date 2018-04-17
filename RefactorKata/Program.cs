5c8d98de5e623c47d1b3483a78dbb8092d58ecc3
Francesca Marciani <fmarciani@gmail.com>
2018-04-17 10:01:09 -0500

    Moved products out of using scope.

---
+++ RefactorKata/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace RefactorKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new List<Product>();
+
+            using (var conn =
+                new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;"))
+            {
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from Products";
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var prod = new Product();
+                    prod.Name = reader["Name"].ToString();
+                    products.Add(prod);
+                }
+            }
+
+            Console.WriteLine("Products Loaded!");
+            
+            foreach (var product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
